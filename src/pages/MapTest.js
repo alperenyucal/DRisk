da3aace31e3c9b58fdcfd0eebcbc87f552276384
Zülal <akkanz16@itu.edu.tr>
2019-12-13 02:52:33 +0300

    Added map files

--- src/pages/MapTest.js
+++ src/pages/MapTest.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Map from "../components/Map"
 import Region from "../components/Region"
 
-let regions = "";
 
-export default ()=>{
+export default () => {
+
+  let [regions, setRegions] = useState([]);
+
+  useEffect(() => {
+    fetch("/static/map.json")
+      .then(res => res.json())
+      .then(data => {
+        let temp = data.regions.map((region) => {
+          return {
+            id: region.id,
+            name: region.name,
+            nodes: region.nodes.map((node) => { return { x: (node.x * 40), y: (node.y * 30) } })
+          }
+        })
+
+
+        setRegions(temp);
+      })
+  })
+
   return (
     <Map width="1200" height="600">
       {
-        regions.map((region)=>
-        {
+        regions.map((region) => {
           return <Region key={region.id} lineColor="black" width="2" fillColor="gray" regionName={region.name} nodes={region.nodes} />
         }
         )
